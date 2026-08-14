dde9c5c5bb31a086458d7ede9e629c4f574517c3
Laith98Dev <7addad0asd@gmail.com>
2026-08-15 01:44:38 +0300

    Bedrock 1.26.44
    SetScorePacket removal entries wrap the objective name in a second
    optional. The network protocol version is unchanged at 2168.

--- src/SetScorePacket.php
+++ src/SetScorePacket.php
@@ -59,7 +59,8 @@ class SetScorePacket extends DataPacket implements ClientboundPacket{
 			$entry->scoreboardId = VarInt::readSignedLong($in);
 			switch($entry->type){
 				case ScorePacketEntry::TYPE_REMOVE:
-					$entry->objectiveName = CommonTypes::readOptional($in, CommonTypes::getString(...));
+					//1.26.44: the objective name is wrapped in a second optional
+					$entry->objectiveName = CommonTypes::readOptional($in, fn(ByteBufferReader $in) => CommonTypes::readOptional($in, CommonTypes::getString(...)));
 					break;
 				case ScorePacketEntry::TYPE_PLAYER:
 				case ScorePacketEntry::TYPE_ENTITY:
@@ -91,7 +92,7 @@ class SetScorePacket extends DataPacket implements ClientboundPacket{
 			VarInt::writeSignedLong($out, $entry->scoreboardId);
 			switch($entryType){
 				case ScorePacketEntry::TYPE_REMOVE:
-					CommonTypes::writeOptional($out, $entry->objectiveName, CommonTypes::putString(...));
+					CommonTypes::writeOptional($out, $entry->objectiveName, fn(ByteBufferWriter $out, string $v) => CommonTypes::writeOptional($out, $v, CommonTypes::putString(...)));
 					break;
 				case ScorePacketEntry::TYPE_PLAYER:
 				case ScorePacketEntry::TYPE_ENTITY:
