32fdc271f89ab7b7d6f0b6f05917eb445921fd70
Laith98Dev <7addad0asd@gmail.com>
2026-09-01 22:15:29 +0300

    v1.26.45 Changes

--- src/SetScorePacket.php
+++ src/SetScorePacket.php
@@ -60,7 +60,7 @@ class SetScorePacket extends DataPacket implements ClientboundPacket{
 			switch($entry->type){
 				case ScorePacketEntry::TYPE_REMOVE:
 					//1.26.44: the objective name is wrapped in a second optional
-					$entry->objectiveName = CommonTypes::readDoubleOptional($in, CommonTypes::getString(...));
+					$entry->objectiveName = CommonTypes::readOptional($in, CommonTypes::getString(...));
 					break;
 				case ScorePacketEntry::TYPE_PLAYER:
 				case ScorePacketEntry::TYPE_ENTITY:
@@ -92,7 +92,7 @@ class SetScorePacket extends DataPacket implements ClientboundPacket{
 			VarInt::writeSignedLong($out, $entry->scoreboardId);
 			switch($entryType){
 				case ScorePacketEntry::TYPE_REMOVE:
-					CommonTypes::writeDoubleOptional($out, $entry->objectiveName, CommonTypes::putString(...));
+					CommonTypes::writeOptional($out, $entry->objectiveName, CommonTypes::putString(...));
 					break;
 				case ScorePacketEntry::TYPE_PLAYER:
 				case ScorePacketEntry::TYPE_ENTITY:
